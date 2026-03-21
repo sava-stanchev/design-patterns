124759ac003af2d6c742c0484d4e0d0d4fa69315
sava-stanchev <s.stanchev94@gmail.com>
2026-03-21 14:24:22 +0200

    add dynamic behavior switching with Strategy pattern

--- src/strategy/ducksimulator/MiniDuckSimulator.java
+++ src/strategy/ducksimulator/MiniDuckSimulator.java
@@ -1,9 +1,16 @@
 package strategy.ducksimulator;
 
+import strategy.ducksimulator.fly.FlyRocketPowered;
+
 public class MiniDuckSimulator {
     public static void main(String[] args) {
         Duck mallard = new MallardDuck();
         mallard.performQuack();
         mallard.performFly();
+
+        Duck model = new ModelDuck();
+        model.performFly();
+        model.setFlyBehavior(new FlyRocketPowered());
+        model.performFly();
     }
 }
---
+++ src/strategy/ducksimulator/ModelDuck.java
@@ -0,0 +1,16 @@
+package strategy.ducksimulator;
+
+import strategy.ducksimulator.fly.FlyNoWay;
+import strategy.ducksimulator.quack.Quack;
+
+public class ModelDuck extends Duck {
+
+    public ModelDuck() {
+        flyBehavior = new FlyNoWay();
+        quackBehavior = new Quack();
+    }
+
+    public void display() {
+        System.out.println("I'm a model duck");
+    }
+}
