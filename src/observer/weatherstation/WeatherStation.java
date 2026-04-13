bea5a33bbb8b8f7cdcada272116f4a989843a81b
sava-stanchev <s.stanchev94@gmail.com>
2026-04-14 02:25:28 +0300

    update WeatherStation driver for Observer pattern implementation

--- src/observer/weatherstation/WeatherStation.java
+++ src/observer/weatherstation/WeatherStation.java
@@ -2,10 +2,13 @@ package observer.weatherstation;
 
 public class WeatherStation {
     public static void main(String[] args) {
-        CurrentConditionsDisplay display = new CurrentConditionsDisplay();
+        WeatherData weatherData = new WeatherData();
 
-        WeatherData weatherData = new WeatherData(display);
+        CurrentConditionsDisplay currentDisplay =
+                new CurrentConditionsDisplay(weatherData);
 
         weatherData.setMeasurements(80, 65, 30.4f);
+        weatherData.setMeasurements(82, 70, 29.2f);
+        weatherData.setMeasurements(78, 90, 29.2f);
     }
 }
