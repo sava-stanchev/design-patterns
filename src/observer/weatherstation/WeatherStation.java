5aa03ed04b106b11af34b346e59090ca89d0caa6
sava-stanchev <s.stanchev94@gmail.com>
2026-04-14 02:34:24 +0300

    add HeatIndexDisplay observer to weather station

--- src/observer/weatherstation/WeatherStation.java
+++ src/observer/weatherstation/WeatherStation.java
@@ -7,6 +7,9 @@ public class WeatherStation {
         CurrentConditionsDisplay currentDisplay =
                 new CurrentConditionsDisplay(weatherData);
 
+        HeatIndexDisplay heatIndexDisplay =
+                new HeatIndexDisplay(weatherData);
+
         weatherData.setMeasurements(80, 65, 30.4f);
         weatherData.setMeasurements(82, 70, 29.2f);
         weatherData.setMeasurements(78, 90, 29.2f);
