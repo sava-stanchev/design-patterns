7410a1c6dfa9ac3815bb63ccbd6c5e9118284e9b
sava-stanchev <s.stanchev94@gmail.com>
2026-04-16 22:13:14 +0300

    add demo cases for size-based beverage pricing

--- src/decorator/starbuzz/StarbuzzCoffee.java
+++ src/decorator/starbuzz/StarbuzzCoffee.java
@@ -2,30 +2,44 @@ package decorator.starbuzz;
 
 public class StarbuzzCoffee {
     public static void main(String[] args) {
-        Beverage beverage = new Espresso();
-        System.out.println(
-                beverage.getDescription()
-                        + " $" + beverage.cost()
-        );
+        Beverage beverage1 = new Espresso();
+        beverage1.setSize(Beverage.Size.TALL);
+
+        System.out.println(beverage1.getDescription()
+                + " | " + beverage1.getSize()
+                + " | $" + beverage1.cost());
 
         Beverage beverage2 = new DarkRoast();
+        beverage2.setSize(Beverage.Size.GRANDE);
+
         beverage2 = new Mocha(beverage2);
         beverage2 = new Mocha(beverage2);
         beverage2 = new Whip(beverage2);
 
-        System.out.println(
-                beverage2.getDescription()
-                        + " $" + beverage2.cost()
-        );
+        System.out.println(beverage2.getDescription()
+                + " | " + beverage2.getSize()
+                + " | $" + beverage2.cost());
 
         Beverage beverage3 = new HouseBlend();
+        beverage3.setSize(Beverage.Size.VENTI);
+
         beverage3 = new Soy(beverage3);
+        beverage3 = new Mocha(beverage3);
         beverage3 = new Milk(beverage3);
         beverage3 = new Whip(beverage3);
 
-        System.out.println(
-                beverage3.getDescription()
-                        + " $" + beverage3.cost()
-        );
+        System.out.println(beverage3.getDescription()
+                + " | " + beverage3.getSize()
+                + " | $" + beverage3.cost());
+
+        Beverage beverage4 = new Decaf();
+        beverage4.setSize(Beverage.Size.TALL);
+
+        beverage4 = new Soy(beverage4);
+        beverage4 = new Whip(beverage4);
+
+        System.out.println(beverage4.getDescription()
+                + " | " + beverage4.getSize()
+                + " | $" + beverage4.cost());
     }
 }
