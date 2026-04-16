4ca103d4ef1b17b2e7c50b6c882824800ecb1b06
sava-stanchev <s.stanchev94@gmail.com>
2026-04-16 22:00:24 +0300

    delegate beverage size through condiment decorators

--- src/decorator/starbuzz/CondimentDecorator.java
+++ src/decorator/starbuzz/CondimentDecorator.java
@@ -1,5 +1,12 @@
 package decorator.starbuzz;
 
 public abstract class CondimentDecorator extends Beverage {
+    protected Beverage beverage;
+
+    @Override
+    public Size getSize() {
+        return beverage.getSize();
+    }
+
     public abstract String getDescription();
 }
