cb4779d821f826433ced85865a1ba15e51d658c9
sava-stanchev <s.stanchev94@gmail.com>
2026-04-16 21:58:31 +0300

    add beverage size support to Beverage base class

--- src/decorator/starbuzz/Beverage.java
+++ src/decorator/starbuzz/Beverage.java
@@ -1,10 +1,23 @@
 package decorator.starbuzz;
 
 public abstract class Beverage {
+    public enum Size {
+        TALL, GRANDE, VENTI
+    }
+
     String description = "Unknown Beverage";
+    Size size = Size.TALL;
 
     public String getDescription() {
         return description;
+    }
+
+    public void setSize(Size size) {
+        this.size = size;
+    }
+
+    public Size getSize() {
+        return size;
     }
 
     public abstract double cost();
