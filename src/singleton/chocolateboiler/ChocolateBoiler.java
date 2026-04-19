e162951bc465f9bfe3a181f2d23a222f68695708
sava-stanchev <s.stanchev94@gmail.com>
2026-04-19 21:37:21 +0300

    convert ChocolateBoiler to singleton and update usage

--- src/singleton/chocolateboiler/ChocolateBoiler.java
+++ src/singleton/chocolateboiler/ChocolateBoiler.java
@@ -4,9 +4,19 @@ public class ChocolateBoiler {
     private boolean empty;
     private boolean boiled;
 
-    public ChocolateBoiler() {
+    private static ChocolateBoiler uniqueInstance;
+
+    private ChocolateBoiler() {
         empty = true;
         boiled = false;
+    }
+
+    public static ChocolateBoiler getInstance() {
+        if (uniqueInstance == null) {
+            uniqueInstance = new ChocolateBoiler();
+        }
+
+        return uniqueInstance;
     }
 
     public void fill() {
--- src/singleton/chocolateboiler/ChocolateBoilerTest.java
+++ src/singleton/chocolateboiler/ChocolateBoilerTest.java
@@ -2,10 +2,13 @@ package singleton.chocolateboiler;
 
 public class ChocolateBoilerTest {
     public static void main(String[] args) {
-        ChocolateBoiler boiler1 = new ChocolateBoiler();
-        ChocolateBoiler boiler2 = new ChocolateBoiler();
+        ChocolateBoiler boiler1 = ChocolateBoiler.getInstance();
+        ChocolateBoiler boiler2 = ChocolateBoiler.getInstance();
+
+        System.out.println(boiler1 == boiler2);
 
         boiler1.fill();
-        boiler2.fill();
+        boiler2.boil();
+        boiler1.drain();
     }
 }
