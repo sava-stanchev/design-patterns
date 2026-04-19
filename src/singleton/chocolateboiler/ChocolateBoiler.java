9fabbda697cf0f1ade460c9e8f10751cfcd99aa3
sava-stanchev <s.stanchev94@gmail.com>
2026-04-19 21:39:13 +0300

    make ChocolateBoiler singleton thread-safe using synchronized

--- src/singleton/chocolateboiler/ChocolateBoiler.java
+++ src/singleton/chocolateboiler/ChocolateBoiler.java
@@ -11,7 +11,7 @@ public class ChocolateBoiler {
         boiled = false;
     }
 
-    public static ChocolateBoiler getInstance() {
+    public static synchronized ChocolateBoiler getInstance() {
         if (uniqueInstance == null) {
             uniqueInstance = new ChocolateBoiler();
         }
