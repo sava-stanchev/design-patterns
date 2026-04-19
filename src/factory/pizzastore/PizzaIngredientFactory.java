84f758f81eb0612c2ba6ddbd6c569cb8bb989f55
sava-stanchev <s.stanchev94@gmail.com>
2026-04-19 12:27:08 +0300

    add ingredient factory interface

---
+++ src/factory/pizzastore/PizzaIngredientFactory.java
@@ -0,0 +1,12 @@
+package factory.pizzastore;
+
+import factory.pizzastore.ingredients.*;
+
+public interface PizzaIngredientFactory {
+    Dough createDough();
+    Sauce createSauce();
+    Cheese createCheese();
+    Veggies[] createVeggies();
+    Pepperoni createPepperoni();
+    Clams createClam();
+}
