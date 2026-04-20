ac42a3144c67b00e2775d47b3c8fe2f16403bcca
sava-stanchev <s.stanchev94@gmail.com>
2026-04-20 21:24:20 +0300

    add RemoteControl with 7 command slots

---
+++ src/command/remotecontrol/RemoteControl.java
@@ -0,0 +1,11 @@
+package command.remotecontrol;
+
+public class RemoteControl {
+    Command[] onCommands;
+    Command[] offCommands;
+
+    public RemoteControl() {
+        onCommands = new Command[7];
+        offCommands = new Command[7];
+    }
+}
