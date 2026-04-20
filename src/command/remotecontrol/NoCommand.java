2c43dd4a6924a7fcf30e1bfdea7d8bf271435bda
sava-stanchev <s.stanchev94@gmail.com>
2026-04-20 21:27:35 +0300

    add NoCommand and initialize slots

---
+++ src/command/remotecontrol/NoCommand.java
@@ -0,0 +1,8 @@
+package command.remotecontrol;
+
+public class NoCommand implements Command {
+    @Override
+    public void execute() {
+        // do nothing
+    }
+}
--- src/command/remotecontrol/RemoteControl.java
+++ src/command/remotecontrol/RemoteControl.java
@@ -7,5 +7,12 @@ public class RemoteControl {
     public RemoteControl() {
         onCommands = new Command[7];
         offCommands = new Command[7];
+
+        Command noCommand = new NoCommand();
+
+        for (int i = 0; i < 7; i++) {
+            onCommands[i] = noCommand;
+            offCommands[i] = noCommand;
+        }
     }
 }
