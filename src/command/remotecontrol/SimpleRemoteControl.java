a909fb5ac7cea2fbdb0c0d18c92170f04f5d902f
sava-stanchev <s.stanchev94@gmail.com>
2026-04-20 19:18:55 +0300

    add SimpleRemoteControl invoker

---
+++ src/command/remotecontrol/SimpleRemoteControl.java
@@ -0,0 +1,13 @@
+package command.remotecontrol;
+
+public class SimpleRemoteControl {
+    private Command slot;
+
+    public void setCommand(Command command) {
+        this.slot = command;
+    }
+
+    public void buttonWasPressed() {
+        slot.execute();
+    }
+}
