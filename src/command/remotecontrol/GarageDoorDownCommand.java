fcdbbe1aa9071ca8dc072d794a25bc712ca7efc6
sava-stanchev <s.stanchev94@gmail.com>
2026-04-20 21:46:58 +0300

    add GarageDoorDownCommand

---
+++ src/command/remotecontrol/GarageDoorDownCommand.java
@@ -0,0 +1,14 @@
+package command.remotecontrol;
+
+public class GarageDoorDownCommand implements Command {
+    private GarageDoor garageDoor;
+
+    public GarageDoorDownCommand(GarageDoor garageDoor) {
+        this.garageDoor = garageDoor;
+    }
+
+    @Override
+    public void execute() {
+        garageDoor.down();
+    }
+}
