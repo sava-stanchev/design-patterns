287a9a6e01174281f60c7d6f0a4125d9dce98b78
sava-stanchev <s.stanchev94@gmail.com>
2026-04-20 20:13:53 +0300

    add test class to execute basic command pattern flow

---
+++ src/command/remotecontrol/RemoteControlTest.java
@@ -0,0 +1,12 @@
+package command.remotecontrol;
+
+public class RemoteControlTest {
+    public static void main(String[] args) {
+        SimpleRemoteControl remote = new SimpleRemoteControl();
+        Light light = new Light();
+        Command lightOn = new LightOnCommand(light);
+
+        remote.setCommand(lightOn);
+        remote.buttonWasPressed();
+    }
+}
