44cdf3a0500afb12adae0868c9ea08f0375f737a
sava-stanchev <s.stanchev94@gmail.com>
2026-04-20 21:30:27 +0300

    add slot assignment, button handling and remote display

--- src/command/remotecontrol/RemoteControl.java
+++ src/command/remotecontrol/RemoteControl.java
@@ -15,4 +15,32 @@ public class RemoteControl {
             offCommands[i] = noCommand;
         }
     }
+
+    public void setCommand(int slot, Command onCommand, Command offCommand) {
+        onCommands[slot] = onCommand;
+        offCommands[slot] = offCommand;
+    }
+
+    public void onButtonWasPushed(int slot) {
+        onCommands[slot].execute();
+    }
+
+    public void offButtonWasPushed(int slot) {
+        offCommands[slot].execute();
+    }
+
+    public String toString() {
+        StringBuilder stringBuff = new StringBuilder();
+        stringBuff.append("\n------ Remote Control ------\n");
+
+        for (int i = 0; i < onCommands.length; i++) {
+            stringBuff.append("[slot " + i + "] "
+                    + onCommands[i].getClass().getName()
+                    + "    "
+                    + offCommands[i].getClass().getName()
+                    + "\n");
+        }
+
+        return stringBuff.toString();
+    }
 }
