e95ea870787a1e6bbc23e41596b33942e4a12454
sava-stanchev <s.stanchev94@gmail.com>
2026-04-20 20:23:02 +0300

    add GarageDoorOpenCommand implementation

---
+++ src/command/remotecontrol/GarageDoorOpenCommand.java
@@ -0,0 +1,14 @@
+package command.remotecontrol;
+
+public class GarageDoorOpenCommand implements Command {
+    private GarageDoor garageDoor;
+
+    public GarageDoorOpenCommand(GarageDoor garageDoor) {
+        this.garageDoor = garageDoor;
+    }
+
+    @Override
+    public void execute() {
+        garageDoor.up();
+    }
+}
