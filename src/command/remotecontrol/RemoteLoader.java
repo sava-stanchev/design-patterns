4192dd0ac5c609bcf0bd53d561b9eae4e2439c4c
sava-stanchev <s.stanchev94@gmail.com>
2026-04-21 19:03:00 +0300

    demonstrate undo func in RemoteLoader

--- src/command/remotecontrol/RemoteLoader.java
+++ src/command/remotecontrol/RemoteLoader.java
@@ -36,14 +36,18 @@ public class RemoteLoader {
         // simulate button presses
         remoteControl.onButtonWasPushed(0);
         remoteControl.offButtonWasPushed(0);
+        remoteControl.undoButtonWasPushed();
 
         remoteControl.onButtonWasPushed(1);
         remoteControl.offButtonWasPushed(1);
+        remoteControl.undoButtonWasPushed();
 
         remoteControl.onButtonWasPushed(2);
         remoteControl.offButtonWasPushed(2);
+        remoteControl.undoButtonWasPushed();
 
         remoteControl.onButtonWasPushed(3);
         remoteControl.offButtonWasPushed(3);
+        remoteControl.undoButtonWasPushed();
     }
 }
