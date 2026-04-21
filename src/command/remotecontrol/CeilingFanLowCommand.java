dfcebfc62e1d13f7e509fec74748b47aab26cbf0
sava-stanchev <s.stanchev94@gmail.com>
2026-04-21 21:17:11 +0300

    add CeilingFan medium, low, and off commands with undo

---
+++ src/command/remotecontrol/CeilingFanLowCommand.java
@@ -0,0 +1,39 @@
+package command.remotecontrol;
+
+public class CeilingFanLowCommand implements Command {
+    private CeilingFan ceilingFan;
+    private int prevSpeed;
+
+    public CeilingFanLowCommand(CeilingFan ceilingFan) {
+        this.ceilingFan = ceilingFan;
+    }
+
+    @Override
+    public void execute() {
+        prevSpeed = ceilingFan.getSpeed();
+        ceilingFan.low();
+    }
+
+    @Override
+    public void undo() {
+        restore(prevSpeed);
+    }
+
+    private void restore(int speed) {
+        switch (speed) {
+            case CeilingFan.HIGH:
+                ceilingFan.high();
+                break;
+            case CeilingFan.MEDIUM:
+                ceilingFan.medium();
+                break;
+            case CeilingFan.LOW:
+                ceilingFan.low();
+                break;
+            case CeilingFan.OFF:
+            default:
+                ceilingFan.off();
+                break;
+        }
+    }
+}
---
+++ src/command/remotecontrol/CeilingFanOffCommand.java
@@ -0,0 +1,39 @@
+package command.remotecontrol;
+
+public class CeilingFanOffCommand implements Command {
+    private CeilingFan ceilingFan;
+    private int prevSpeed;
+
+    public CeilingFanOffCommand(CeilingFan ceilingFan) {
+        this.ceilingFan = ceilingFan;
+    }
+
+    @Override
+    public void execute() {
+        prevSpeed = ceilingFan.getSpeed();
+        ceilingFan.off();
+    }
+
+    @Override
+    public void undo() {
+        restore(prevSpeed);
+    }
+
+    private void restore(int speed) {
+        switch (speed) {
+            case CeilingFan.HIGH:
+                ceilingFan.high();
+                break;
+            case CeilingFan.MEDIUM:
+                ceilingFan.medium();
+                break;
+            case CeilingFan.LOW:
+                ceilingFan.low();
+                break;
+            case CeilingFan.OFF:
+            default:
+                ceilingFan.off();
+                break;
+        }
+    }
+}
