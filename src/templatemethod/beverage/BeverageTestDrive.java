d0cc38ac2d497a8f0e0556837a97dba26c981075
sava-stanchev <s.stanchev94@gmail.com>
2026-04-22 22:51:30 +0300

    add test driver for Template Method pattern

---
+++ src/templatemethod/beverage/BeverageTestDrive.java
@@ -0,0 +1,14 @@
+package templatemethod.beverage;
+
+public class BeverageTestDrive {
+    public static void main(String[] args) {
+        Tea tea = new Tea();
+        Coffee coffee = new Coffee();
+
+        System.out.println("Making tea...");
+        tea.prepareRecipe();
+
+        System.out.println("\nMaking coffee...");
+        coffee.prepareRecipe();
+    }
+}
