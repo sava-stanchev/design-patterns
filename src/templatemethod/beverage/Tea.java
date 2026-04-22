c9968646456178c572a30e6c7a1ed73291c928de
sava-stanchev <s.stanchev94@gmail.com>
2026-04-22 22:45:44 +0300

    add Tea and Coffee concrete implementations of CaffeineBeverage

---
+++ src/templatemethod/beverage/Tea.java
@@ -0,0 +1,13 @@
+package templatemethod.beverage;
+
+public class Tea extends CaffeineBeverage {
+    @Override
+    protected void brew() {
+        System.out.println("Steeping the tea");
+    }
+
+    @Override
+    protected void addCondiments() {
+        System.out.println("Adding lemon");
+    }
+}
