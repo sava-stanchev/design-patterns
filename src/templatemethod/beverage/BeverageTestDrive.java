ded04e57e96d2abbc1cbf803efd767d5628dcf02
sava-stanchev <s.stanchev94@gmail.com>
2026-04-23 23:02:32 +0300

    add CoffeeWithHook in test driver to demonstrate hook behavior

--- src/templatemethod/beverage/BeverageTestDrive.java
+++ src/templatemethod/beverage/BeverageTestDrive.java
@@ -3,12 +3,16 @@ package templatemethod.beverage;
 public class BeverageTestDrive {
     public static void main(String[] args) {
         Tea tea = new Tea();
-        CoffeeWithHook coffee = new CoffeeWithHook();
+        Coffee coffee = new Coffee();
+        CoffeeWithHook coffeeWithHook = new CoffeeWithHook();
 
         System.out.println("Making tea...");
         tea.prepareRecipe();
 
         System.out.println("\nMaking coffee...");
         coffee.prepareRecipe();
+
+        System.out.println("\nMaking coffeeWithHook...");
+        coffeeWithHook.prepareRecipe();
     }
 }
