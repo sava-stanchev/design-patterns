47d389f1aba0b6c408417159a346794193a42d12
sava-stanchev <s.stanchev94@gmail.com>
2026-04-24 20:44:12 +0300

    add Duck class implementing Comparable for sorting

---
+++ src/templatemethod/ducks/Duck.java
@@ -0,0 +1,27 @@
+package templatemethod.ducks;
+
+public class Duck implements Comparable<Duck> {
+    private final String name;
+    private final int weight;
+
+    public Duck(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return name + " weighs " + weight;
+    }
+
+    @Override
+    public int compareTo(Duck otherDuck) {
+        if (this.weight < otherDuck.weight) {
+            return -1;
+        } else if (this.weight == otherDuck.weight) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+}
