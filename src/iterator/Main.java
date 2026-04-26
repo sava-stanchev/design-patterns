e441dd8f37670970d41a2e584b6c06466a2209f1
sava-stanchev <s.stanchev94@gmail.com>
2026-04-26 20:33:03 +0300

    update Main to use Menu abstraction

--- src/iterator/Main.java
+++ src/iterator/Main.java
@@ -2,8 +2,8 @@ package iterator;
 
 public class Main {
     public static void main(String[] args) {
-        PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
-        DinerMenu dinerMenu = new DinerMenu();
+        Menu pancakeHouseMenu = new PancakeHouseMenu();
+        Menu dinerMenu = new DinerMenu();
 
         Waitress waitress = new Waitress(pancakeHouseMenu, dinerMenu);
 
