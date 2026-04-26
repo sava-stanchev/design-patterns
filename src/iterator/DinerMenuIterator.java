bbe846dc4ff35952271a172aba0cfd729cb02b5d
sava-stanchev <s.stanchev94@gmail.com>
2026-04-26 20:22:58 +0300

    update DinerMenuIterator to implement java.util.Iterator

--- src/iterator/DinerMenuIterator.java
+++ src/iterator/DinerMenuIterator.java
@@ -1,6 +1,8 @@
 package iterator;
 
-public class DinerMenuIterator implements Iterator {
+import java.util.Iterator;
+
+public class DinerMenuIterator implements Iterator<MenuItem> {
     MenuItem[] items;
     int position = 0;
 
@@ -13,8 +15,11 @@ public class DinerMenuIterator implements Iterator {
     }
 
     public MenuItem next() {
-        MenuItem menuItem = items[position];
-        position = position + 1;
-        return menuItem;
+        return items[position++];
+    }
+
+    public void remove() {
+        throw new UnsupportedOperationException(
+                "You shouldn't be trying to remove menu items.");
     }
 }
